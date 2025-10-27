125b51b8f6add9004c113babd33aa9f0a165e223
Leandro Langlois <leandrocmlanglois@gmail.com>
2025-10-27 15:01:29 -0300

    chore(tests): url atualizada para testes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('carregar notícias do editor', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('https://equipe-5-vert.vercel.app/');
   await expect(page.getByTestId("noticia")).toBeVisible()
 });
 
 test('entrar na noticia' , async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('https://equipe-5-vert.vercel.app/');
   await page.getByTestId("noticia").click()
   await expect(page.getByText("Detalhes da Notícia")).toBeVisible()
 });
 
 test('criar noticia' , async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('https://equipe-5-vert.vercel.app/');
   await page.getByTestId("criar-noticia").click()
 });
