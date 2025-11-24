d0f973e3ddea8c606d0d23f79f8bca16dc99fef0
Giuliano <giuli@tutanota.de>
2025-11-24 16:24:52 -0300

    fix(tests): multiplas noticias

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,14 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test('carregar notícias do editor', async ({ page }) => {
   await page.goto('https://equipe-5-vert.vercel.app/');
-  await expect(page.getByTestId("noticia")).toBeVisible()
+  await expect(page.getByTestId("noticia")).toBeVisible();
 });
 
 test('entrar na noticia' , async ({ page }) => {
   await page.goto('https://equipe-5-vert.vercel.app/');
-  await page.getByTestId("noticia").click()
+  await page.getByTestId("noticia").first().click();
 });
 
 test('criar noticia' , async ({ page }) => {
   await page.goto('https://equipe-5-vert.vercel.app/');
+  await page.getByTestId("criar-noticia").click();
 });
