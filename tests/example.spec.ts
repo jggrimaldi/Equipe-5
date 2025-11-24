2f698c88dc95af8ccc144c6a3b5f36143d6a2d7f
Leandro Langlois <leandrocmlanglois@gmail.com>
2025-11-24 19:13:05 -0300

    fix(tests): teste entrar na noticia

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('carregar notícias do editor', async ({ page }) => {
   await page.goto('https://equipe-5-vert.vercel.app/');
-  await expect(page.getByTestId("noticia")).toBeVisible();
+  await expect(page.getByTestId("noticia").first()).toBeVisible();
 });
 
 test('entrar na noticia' , async ({ page }) => {
